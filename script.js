95cfa2b16abcf0470f18b0e173e01220334a93e5
Jeeko Arangcon <121530905+arangconjeeko@users.noreply.github.com>
2022-12-31 02:59:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 65
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 65
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 13
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 387.6923076923077)
+}
+
+setClock()
